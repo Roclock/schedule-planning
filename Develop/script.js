09ed07f6989159eee52622363fa634ea261ebd9f
unknown <jeffrey1134@gmail.com>
2022-10-16 15:25:58 -0700

    added javascript hour checker

--- Develop/script.js
+++ Develop/script.js
@@ -8,5 +8,13 @@ var timedisplayEl = document.querySelector("#currentDay")
 setInterval(function()  {
     timedisplayEl.textContent = moment().format('MMMM Do YYYY, h:mm:ss a');
     
-}, 1000); //Function to make time current at all times.
+}, 1000); //use of moment.js function to make time current at all times in header.
 
+function trackHours() {
+ var currentMoment = moment().hour();
+ $('.time-block').each(function () {
+    var timeHour = parseInt($(this).attr(id).split("hour")[1]);
+    console.log(timeHour,currentMoment)
+    //Check if we've moved past this hour, set up if else statement underneath for classes of Past, Current, or Future.
+ })
+}
